2ab6e710417d3dec3571c4f6b968d280caae8e7b
ivanocortesini <ivano.cortesini@gmail.com>
2016-12-13 19:05:59 +0100

    ...

---
+++ amis-policy-metadata/src/main/java/org/fao/policy/metadata/MetadataBuilder.java
@@ -0,0 +1,16 @@
+package org.fao.policy.metadata;
+
+import org.fao.amis.policy.dto.full.Policy;
+import org.fao.fenix.commons.msd.dto.full.DSDDataset;
+import org.fao.fenix.commons.msd.dto.full.MeIdentification;
+
+public class MetadataBuilder {
+
+    public MeIdentification<DSDDataset> getMetadata(Policy policy) throws Exception {
+        return null; //TODO
+    }
+
+    public String getMetadataId(Policy policy) throws Exception {
+        return null; //TODO
+    }
+}
